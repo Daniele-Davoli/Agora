742b369f95496dce28d44732b9c22568ab2c0f18
Daniele Davoli <118203866+Daniele-Davoli@users.noreply.github.com>
2024-06-02 22:21:09 +0200

    Updated

---
+++ MainServer/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+// Servire i file statici dalla cartella 'public'
+app.use(express.static('public'));
+
+// Route principale
+app.get('/', async (req, res) => {
+    res.sendFile(__dirname + '/public/main.html');
+});
+
+// Avviare il server
+app.listen(port, () => {
+  console.log(`Server in esecuzione all'indirizzo http://localhost:${port}/`);
+});
