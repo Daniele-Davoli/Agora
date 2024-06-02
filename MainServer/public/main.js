8d75d504f69d9e4948640dc05947f3cc276827a4
Daniele Davoli <118203866+Daniele-Davoli@users.noreply.github.com>
2024-06-02 23:50:36 +0200

    First Boot

--- MainServer/public/main.js
+++ MainServer/public/main.js
@@ -1,3 +1,25 @@
+flag=false;
+let intervall;
+
 window.onload=()=>{
-    
+}
+
+
+
+function si(){
+    clearInterval(intervall);
+    document.body.style.backgroundColor="rgb(33,33,33)";
+    setTimeout(()=>{
+        alert("ti amo anche io");
+    },100)
+
+}
+
+function no(){
+    intervall=setInterval(()=>{
+        if(flag)document.body.style.backgroundColor="red";
+        else document.body.style.backgroundColor="blue";
+
+        flag=!flag;
+    },100);
 }
