88bce7de4f1ffb3e844a8fab9e4961f4fe0d7ad7
Daniele <118203866+Daniele-Davoli@users.noreply.github.com>
2024-09-17 12:23:18 +0200

    Update passport-setup.js

--- MainServer/passport-setup.js
+++ MainServer/passport-setup.js
@@ -2,8 +2,8 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(new GoogleStrategy({
-    clientID: '382797113950-puuvr948htop43ii77t4bn99966smdf6.apps.googleusercontent.com',
-    clientSecret: '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',
+    clientID: '',
+    clientSecret: '',
     callbackURL: '/auth/google/callback'
 },function(token, tokenSecret, profile, done) {
 
